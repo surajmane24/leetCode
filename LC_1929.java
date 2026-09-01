bb4279eb364c1ca4ed9b238c8cd9d94553bc275a
Suraj S. Mane <83125278+surajmane24@users.noreply.github.com>
2026-09-01 19:38:33 +0530

    Update LC_1929.java

--- LC_1929.java
+++ LC_1929.java
@@ -1,1 +1,14 @@
 
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] newReturn = new int[2*nums.length];
+        for(int i=0; i < nums.length; i++){
+            newReturn[i] = nums[i];
+        }
+        int newIndex = nums.length;
+        for(int i=0; i < nums.length; i++){
+            newReturn[newIndex++] = nums[i];
+        }
+        return newReturn;
+    }
+}
